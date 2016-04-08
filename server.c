a8614acb70b33b1d11ff183dd4404f69b1a407de
Duy Nguyen <nduytg@gmail.com>
2016-04-08 16:43:43 +0700

    Added files via upload

--- server.c
+++ server.c
@@ -24,7 +24,7 @@ typedef struct Type
 	int name;			//Ma danh dau chuyen xe
 	int type;			//Ma loai ve
 	int price;			//Gia tien
-}
+};
 
 typedef struct Route
 {
@@ -32,10 +32,9 @@ typedef struct Route
 	int n_type_A;		//So ve loai A, B, C
 	int n_type_B;
 	int n_type_C;
-}
+};
 
-void help();
-
+void usage();
 
 void *do_nothing(void *null)
 {
@@ -45,74 +44,11 @@ void *do_nothing(void *null)
 }
 
 
-void handle_request()
-{
-}
+void handle_request(int cli_socket);
 
-int createTCPserverSocket(char *host, int port)
-{	
-	int mySocket;
-	struct addrinfo hints, *p;
-	
-	memset(&hint, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;		//hay AF_INET cung dc
-	hints.ai_socktype = SOCK_STREAM;
-	
-	if((getaddrinfo(host, port, &hints, &p)) != 0)
-	{
-		printf("Failed to getaddrinfo()\n");
-		return -1;
-	}
-	
-	if( (mySocket = socket(p->ai_family, p->ai_socktype, p->ai_protocol);) == -1)
-	{
-		printf("Failed to create socket()\n");
-		return -1;
-	}	
-	
-	if ( (bind(server_socket, p->ai_addr, p->ai_addrlen)) < 0 )
-	{
-		close(mySocket);
-		printf("Failed to bind socket\n");
-		return -1;
-	}	
-	
-	freeaddrinfo(hints);
-	return mySocket;
-}
-
-int createTCPsocket(char *host, int port)
-{	
-	int mySocket;
-	struct addrinfo hints, *p;
-	
-	memset(&hint, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;		//hay AF_INET cung dc
-	hints.ai_socktype = SOCK_STREAM;
-	
-	if((getaddrinfo(host, port, &hints, &p)) != 0)
-	{
-		printf("Failed to getaddrinfo()\n");
-		return -1;
-	}
-	
-	if( (mySocket = socket(p->ai_family, p->ai_socktype, p->ai_protocol);) == -1)
-	{
-		printf("Failed to create socket()\n");
-		return -1;
-	}	
-	
-	freeaddrinfo(hints);
-	return mySocket;
-}
-
-
-
-int acceptTCPsocket()
-{
-	
-	return -1;
-}
+int createTCPserverSocket(char *host, int port, struct addrinfo hints);
+int createTCPsocket();
+int acceptTCPsocket(int serverSock, struct sockaddr_storage addr);
 
 
 //MO HINH 2: lap song song theo phien lam viec
@@ -126,28 +62,128 @@ int acceptTCPsocket()
 
 //Ham xuy ly 1 connection
 //close socket cua tieu trinh
+
+
 int main()
 {
 	pthread_t threads[POOL_SIZE];
-	printf("Server cong ty duong sat X\n");
-	
-	
-	
 	int retcode;
 	int server_socket;
 	int ticket_client;
+	struct addrinfo server_hints;
 	
-	//Server socket for listening all requests
-	if ( (server_socket = createTCPserverSocket(LOCAL_HOST,PORT)) == -1 )
-		exit(-1);
+	printf("Server cong ty duong sat X\n");
 	
-	listen(server_socket, BACKLOG);
-	ticket_client = acceptTCPsocket
+	//Create and bind in this function
+	server_socket = createTCPserverSocket(LOCAL_HOST,PORT,hints);
+	listen(serverSocket,BACKLOG);
+	
+	//Accept ket noi tu day
+	while(1)
+	{
+		struct sockaddr_storage client_addr;
+		ticket_client = acceptTCPsocket(server_socket, sockaddr_storage);
+		
+		
+		//Thread -> handle_request
+		
+		//B1: Send thong tin ve cho client
+		
+		//B2: Nhan request tu client
+		
+		//B3: Tinh toan va check semaphore
+		
+		//B4: Tra ket qua (ok hoac ko)
+		//Thread
+	};
 	
 	
+	
+	close(server_socket);
 	
 	exit(0);
 }
+
+
+int createTCPserverSocket(char *host, int port, struct addrinfo hints)
+{	
+	int mySocket;
+	struct addrinfo *p;
+	
+	memset(&hint, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;		//hay AF_INET cung dc
+	hints.ai_socktype = SOCK_STREAM;
+	
+	if(host == NULL)
+	{
+		printf("Set to default host: localhost\n");
+		host = LOCAL_HOST;
+	}
+	
+	if(port <= 1024 || port > 65535)
+	{
+		printf("Invalid port, set to default port: %d\n",PORT);
+		port = port;
+	}
+		
+	
+	if((getaddrinfo(host, port, &hints, &p)) != 0)
+	{
+		printf("Failed to getaddrinfo()\n");
+		return -1;
+	}
+	
+	if( (mySocket = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+	{
+		printf("Failed to create socket()\n");
+		return -1;
+	}	
+	
+	if ( (bind(server_socket, p->ai_addr, p->ai_addrlen)) < 0 )
+	{
+		close(mySocket);
+		printf("Failed to bind socket\n");
+		return -1;
+	}	
+	
+	//freeaddrinfo(hints);
+	return mySocket;
+}
+
+int createTCPsocket()
+{	
+	int mySocket;
+	//struct addrinfo hints;
+	
+	//memset(&hint, 0, sizeof(hints));
+	//hints.ai_family = AF_UNSPEC;		//hay AF_INET cung dc
+	//hints.ai_socktype = SOCK_STREAM;
+	
+	
+	if( (mySocket = socket(AF_UNSPEC, SOCK_STREAM, 0)) == -1)
+	{
+		printf("Failed to create socket()\n");
+		return -1;
+	}	
+	
+	//freeaddrinfo(hints);
+	return mySocket;
+}
+
+
+
+int acceptTCPsocket(int serverSock, struct sockaddr_storage addr)
+{
+	int newSock = accept(serverSock,(struct sockaddr *)&addr, sizeof(addr));
+	if (newSock == -1)
+	{
+		printf("Failed at accept()\n");
+		return -1;
+	}
+	return newSock;
+}
+
+void handle_request(int cli_socket);
 
 
 void threadSample()
