73677398027287a04e513886353396280c92c659
Duy Nguyen <nduytg@gmail.com>
2016-04-09 10:00:20 +0700

    Added files via upload

--- client.c
+++ client.c
@@ -10,58 +10,97 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <pthread.h>
-#include <mutex.h>
-#include <Ticket.h>
+
+//#include <pthread.h>
+//#include <mutex.h>
+//#include <Ticket.h>
 
 #define POOL_SIZE 10				//Thread pool size
 #define LOCAL_HOST "127.0.0.1"		//Local host IP
-#define PORT 60000				//Port mac dinh cho server
+#define PORT 60000					//Port mac dinh cho server
+#define BACKLOG 10
+#define SO_CHUYEN 3
+#define MAXSIZE 4096
 
 typedef struct Type
 {
-	int name;			//Ma danh dau chuyen xe
-	int type;			//Ma loai ve
+	int number;			//So luong loai ve con lai
 	int price;			//Gia tien
-}
+	
+}Type;
 
 typedef struct Route
 {
-	int name;			//Ma danh dau tuyen xe
-	int n_type_A;		//So ve loai A, B, C
-	int n_type_B;
-	int n_type_C;
-}
+	int code;			//Ma danh dau tuyen xe
+	Type l[SO_CHUYEN];
+}Route;
 
-int createTCPsocket(char *host, int port)
+typedef struct Request
+{
+	int route;
+	int type;	type tu 0 toi 2
+	int number;
+}Request;
+
+//Bien toan cuc
+Route HCM_HN, HCM_HUE, HCM_DALAT;
+
+
+int createTCPsocket()
 {	
 	int mySocket;
-	struct addrinfo hints, *p;
+	//struct addrinfo hints;
 	
-	memset(&hint, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;		//hay AF_INET cung dc
-	hints.ai_socktype = SOCK_STREAM;
+	//memset(&hint, 0, sizeof(hints));
+	//hints.ai_family = AF_UNSPEC;		//hay AF_INET cung dc
+	//hints.ai_socktype = SOCK_STREAM;
 	
-	if((getaddrinfo(host, port, &hints, &p)) != 0)
-	{
-		printf("Failed to getaddrinfo()\n");
-		return -1;
-	}
-	
-	if( (mySocket = socket(p->ai_family, p->ai_socktype, p->ai_protocol);) == -1)
+	if( (mySocket = socket(AF_UNSPEC, SOCK_STREAM, 0)) == -1)
 	{
 		printf("Failed to create socket()\n");
 		return -1;
 	}	
 	
-	freeaddrinfo(hints);
+	//freeaddrinfo(hints);
 	return mySocket;
+}
+
+void showInfo()
+{
+	//Show info bieu do tuyen xe, gia ve
 }
 
 
 
 int main()
 {
+	int clientSock;
+	struct addrinfo hints, *p;
 	
+	printf("Hello, this is Ticket Client\n");
+	
+	if( getaddrinfo(LOCAL_HOST,PORT,&hints,&res) != 0)
+	{
+		printf("Failed at getaddrinfo()\n");
+		freeaddrinfo(hints);
+		exit(-1);
+	}
+	
+	clientSock = createTCPsocket();
+	
+	if(connect(clientSock,p->ai_addr, p->ai_addrlen) < 0)
+	{
+		printf("Cannot connect to %s - %d\n",LOCAL_HOST,PORT);
+		freeaddrinfo(hints);
+		exit(-1);
+	}
+	
+	
+	
+	//recv struct
+	
+	//send request
+	
+	//recv result
 	exit(0);
 }
